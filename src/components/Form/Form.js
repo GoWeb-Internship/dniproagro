7693b6738454742e894d9ded11e23f825d224c56
Vasyl Yakymets <Vasia911119@gmail.com>
2022-11-14 21:12:17 +0200

    fix structure

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -8,7 +8,7 @@ import {
 } from 'react-notifications';
 import 'react-notifications/lib/notifications.css';
 import * as yup from 'yup';
-import { sendMessage } from '../../api/telegramApi'
+import { sendMessage } from '../../api/telegramApi';
 import * as s from './Form.module.css';
 import PhoneInput from 'react-phone-input-2';
 import 'react-phone-input-2/lib/style.css';
@@ -154,10 +154,11 @@ export const Form = () => {
   const onSubmit = (data, e) => {
     try {
       e.preventDefault();
-    //   let sendMessage = () => {};
+      //   let sendMessage = () => {};
       // --- TELEGRAM ---
       let text = `<b>Повідомлення з сайту!</b>\n`;
       text += `<b>Відправник: </b> ${data.name}\n`;
+      text += `<b>Телефон: </b> ${data.phone}\n`;
       text += `<b>Пошта: </b> ${data.email}\n`;
       text += `<b>Повідомлення: </b> ${data.message}\n`;
       text += `<b>Форма отримана з:</b>\n`;
