d222e6fd94ea58c1acfe7dd340cea53db2781f1f
Nadiia Khoptiuk <holineinadia@gmail.com>
2022-11-24 14:16:17 +0200

    fix deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,11 +18,11 @@ import { Cultures } from 'views/Cultures/Cultures';
 const { SLOGAN, COMPANY, CULTURES, PERSONNEL, EQUIPMENTS, GALLERY, CONTACTS } =
   anchors;
 
-const IndexPage = ({ data }) => {
-  const chapters = data?.allMarkdownRemark?.nodes;
-  const aboutCompany = chapters.find(
-    ({ frontmatter: { chapter } }) => chapter === 'about_company',
-  )?.frontmatter;
+const IndexPage = () => {
+  // const chapters = data?.allMarkdownRemark?.nodes;
+  // const aboutCompany = chapters.find(
+  //   ({ frontmatter: { chapter } }) => chapter === 'about_company',
+  // )?.frontmatter;
 
   // const slide_img = [
   //   'https://swiperjs.com/demos/images/nature-1.jpg',
@@ -42,7 +42,7 @@ const IndexPage = ({ data }) => {
       {/* <Section id={SLOGAN}></Section> */}
       {/* про компанію */}
       <Section id={COMPANY}>
-        <About aboutCompany={aboutCompany} />
+        {/* <About aboutCompany={aboutCompany} /> */}
       </Section>
       {/* культури */}
       <Cultures />
@@ -64,73 +64,3 @@ const IndexPage = ({ data }) => {
 export default withLayout(IndexPage);
 
 export const Head = () => <title>Home Page</title>;
-
-export const query = graphql`
-  query ($language: String!) {
-    allMarkdownRemark(
-      filter: { frontmatter: { language: { eq: $language } } }
-    ) {
-      nodes {
-        frontmatter {
-          chapter
-          chapter_range
-          content
-          language
-          phone
-          title
-          workers_list {
-            alt
-            description
-            length_of_service
-            photo
-            position
-            worker
-          }
-          location
-          statistics {
-            category
-            units
-            value
-          }
-          reporting {
-            path
-            title
-          }
-          Equipments_list {
-            alt
-            description
-            equipment
-            image
-          }
-          address
-          contacts {
-            department
-            phone
-            telegram
-            viber
-            whatsapp
-          }
-          email
-          photos_list {
-            alt
-            photo
-          }
-          bg_img {
-            alt
-            photo
-          }
-        }
-      }
-    }
-
-    locales: allLocale(filter: { language: { eq: $language } }) {
-      edges {
-        node {
-          ns
-          data
-          language
-        }
-      }
-    }
-  }
-`;
