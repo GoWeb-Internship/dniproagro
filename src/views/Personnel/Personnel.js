cbbd81a0a7bda5d4a3b166a33cf7b0e8db74a7db
Maryna Ostapliuk <marynaum01@gmail.com>
2022-11-25 16:53:17 +0200

    update personnel for gatsby image

--- src/views/Personnel/Personnel.js
+++ src/views/Personnel/Personnel.js
@@ -3,13 +3,15 @@ import { useStaticQuery, graphql } from 'gatsby';
 import { SwiperSlide } from 'swiper/react';
 import { Section } from 'components';
 import { Slider } from 'components/Slider/Slider';
-import { anchors } from 'utils/constants';
 import { useTranslation } from 'gatsby-plugin-react-i18next';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
 export const Personnel = () => {
   const { i18n } = useTranslation();
 
-  const data = useStaticQuery(
+  const {
+    allMarkdownRemark: { nodes },
+  } = useStaticQuery(
     graphql`
       query {
         allMarkdownRemark(
@@ -17,56 +19,59 @@ export const Personnel = () => {
         ) {
           nodes {
             frontmatter {
+              language
               chapter
               title
               workers_list {
                 worker
                 position
-                length_of_service
                 description
+                length_of_service
                 alt
-                photo
+                photo {
+                  childImageSharp {
+                    gatsbyImageData(
+                      placeholder: BLURRED
+                      jpgOptions: { progressive: true }
+                      width: 400
+                      height: 496
+                    )
+                  }
+                }
               }
-              language
             }
           }
         }
       }
     `,
   );
-  const { PERSONNEL } = anchors;
-  const personnel = data?.allMarkdownRemark?.nodes;
-  // console.log(personnel);
-  const workerlist = personnel?.map(
-    ({ frontmatter }) => frontmatter.workers_list,
-  );
-  //   console.log(workerlist);
-  const personnelChapter = personnel?.find(
+
+  const personnel = nodes?.find(
     ({ frontmatter: { language } }) => language === i18n.language,
   )?.frontmatter;
 
-  //   console.log(personnelChapter);
+  const workerlist = personnel?.workers_list;
 
   return (
-    <Section className=" py-5 " id={PERSONNEL}>
+    <Section className=" py-5 " id={personnel?.chapter}>
       <h2 className="mb-[72px] text-4xl font-bold leading-[45px]">
-        {personnelChapter?.title}
+        {personnel?.title}
       </h2>
       <Slider
         slidesPerGroup={1}
         className="items-end md:w-[704px] xl:w-[1076px]"
       >
-        {data &&
-          workerlist[0]?.map(({ photo, alt }, index) => {
+        {nodes &&
+          workerlist.map(({ photo, alt }, index) => {
             return (
               <SwiperSlide key={index}>
                 {({ isActive }) => (
-                  <img
+                  <GatsbyImage
+                    image={getImage(photo)}
+                    alt={alt}
                     className={
                       isActive ? 'h-[495px] w-[417px]' : 'h-[442px] w-[306px]'
                     }
-                    src={photo}
-                    alt={alt}
                   />
                 )}
               </SwiperSlide>
