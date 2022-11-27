26e1fc628493c8e5d33ff40787b8242696f11ce4
Nadiia Khoptiuk <holineinadia@gmail.com>
2022-11-27 09:53:14 +0200

    fix container and warning

--- tailwind.config.js
+++ tailwind.config.js
@@ -78,7 +78,7 @@ module.exports = {
         padding: {
           DEFAULT: '1.25rem',
           sm: '1.25rem',
-          md: '1.25rem',
+          md: '2rem',
           xl: '1.25rem',
         },
       },
