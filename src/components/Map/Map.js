5347254036a2788851ed29fdf9a9ce7bf0487742
Maryna Ostapliuk <marynaum01@gmail.com>
2022-12-03 14:32:24 +0200

    update map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { useTranslation } from 'gatsby-plugin-react-i18next';
 import * as s from './Map.module.css';
 
 const GOOGLE_API_KEY = process.env.GATSBY_GOOGLE_API_KEY;
 
-let center = {
+const defaultcenter = {
   lat: 48.184682,
   lng: 36.240398,
 };
 
-function MyComponent() {
-  const data = useStaticQuery(
+const Map = () => {
+  const { i18n } = useTranslation();
+
+  const {
+    allMarkdownRemark: { nodes },
+  } = useStaticQuery(
     graphql`
       query {
         allMarkdownRemark(
@@ -20,6 +25,7 @@ function MyComponent() {
           nodes {
             frontmatter {
               location
+              language
             }
           }
         }
@@ -27,35 +33,59 @@ function MyComponent() {
     `,
   );
 
-  const location = data.allMarkdownRemark.nodes;
-  const coordinates = location.map(({ frontmatter }) => {
-    return frontmatter.location.split(':')[2].slice(1, -2);
-  });
+  const location = nodes?.find(
+    ({ frontmatter: { language } }) => language === i18n.language,
+  )?.frontmatter;
+  console.log(nodes);
 
-  center = {
-    lat: Number(coordinates.join(',').split(',')[1]),
-    lng: Number(coordinates.join(',').split(',')[0]),
+  const coordinates = location?.location.split(':')[2].slice(1, -2);
+  console.log(coordinates);
+  const center = {
+    lat: Number(coordinates?.split(',')[1]),
+    lng: Number(coordinates?.split(',')[0]),
   };
 
-  //   const [map, setMap] = React.useState(null);
+  const mapRef = React.useRef(undefined);
 
-  //   const onLoad = React.useCallback(function callback(map) {
-  //     const bounds = new window.google.maps.LatLngBounds();
-  //     map.fitBounds(bounds);
-  //     setMap(map);
-  //   }, []);
+  const onLoad = React.useCallback(function callback(map) {
+    mapRef.current = map;
+  }, []);
 
-  //   const onUnmount = React.useCallback(function callback(map) {
-  //     setMap(null);
-  //   }, []);
+  const onUnmount = React.useCallback(function callback(map) {
+    mapRef.current = undefined;
+  }, []);
 
   return (
-    <LoadScript googleMapsApiKey={GOOGLE_API_KEY}>
-      <GoogleMap mapContainerClassName={s.container} center={center} zoom={12}>
-        <Marker position={center} />
-      </GoogleMap>
-    </LoadScript>
+    <>
+      {location ? (
+        <LoadScript googleMapsApiKey={GOOGLE_API_KEY}>
+          <GoogleMap
+            mapContainerClassName={s.container}
+            center={center}
+            zoom={12}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+            language={i18n.language}
+          >
+            <Marker position={center} />
+          </GoogleMap>
+        </LoadScript>
+      ) : (
+        <LoadScript googleMapsApiKey={GOOGLE_API_KEY}>
+          <GoogleMap
+            mapContainerClassName={s.container}
+            center={defaultcenter}
+            zoom={12}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+            language={i18n.language}
+          >
+            <Marker position={defaultcenter} />
+          </GoogleMap>
+        </LoadScript>
+      )}
+    </>
   );
-}
+};
 
-export default React.memo(MyComponent);
+export default Map;
