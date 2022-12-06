b9880b940c8f243d03a90ccafa375bdf32962c31
Vasyl Yakymets <Vasia911119@gmail.com>
2022-12-06 14:27:29 +0200

    add-spiner

---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as s from './Spinner.module.css';
+
+export const Spinner = () => (
+  <article>
+    <div>
+      <div className={s.spinner}></div>
+    </div>
+  </article>
+);
