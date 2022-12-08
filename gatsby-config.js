675ce39879e4ad2d93705bff5dddff84854d1a44
Vasyl Yakymets <Vasia911119@gmail.com>
2022-12-08 20:06:01 +0200

    fix-google-tag-manager-deelay

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,10 @@ const myCustomQueries = {
   lg: '(min-width: 1280px)',
 };
 
+const getGoogleTagManagerId = setTimeout(
+  () => process.env.GOOGLE_TAGMANAGER_ID,
+  3000,
+);
 // require('dotenv').config({
 //   path: `.env.${process.env.NODE_ENV}`,
 // });
@@ -122,7 +126,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-tagmanager',
       options: {
-        id: process.env.GOOGLE_TAGMANAGER_ID,
+        id: getGoogleTagManagerId,
         includeInDevelopment: false,
       },
     },
