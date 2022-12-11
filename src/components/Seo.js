c6f5be6239fec5b74a83366085d19be7888c4bd0
Maryna Ostapliuk <marynaum01@gmail.com>
2022-12-12 00:57:19 +0200

    upd 2 scripts

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -83,48 +83,11 @@ function Seo({ description, title, lang = 'uk', meta = [] }) {
         rel="canonical"
         href="https://lucent-semolina-877c33.netlify.app/"
       />
-      <link rel="preconnect" as="font" href="https://fonts.googleapis.com" />
-      <link
-        rel="preconnect"
-        as="font"
-        href="https://fonts.gstatic.com"
-        crossorigin
-      />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Mulish:wght@400;700&display=swap"
-        rel="stylesheet"
-      />
+
       <link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon} />
       <link rel="icon" type="image/png" sizes="32x32" href={faviconBig} />
       <link rel="icon" type="image/png" sizes="16x16" href={faviconSmall} />
       {/* <link rel="manifest" href={manifest} /> */}
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '605102281370010');
-        fbq('track', 'PageView');
-          `,
-        }}
-      />
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-        function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-5X9VH2G');
-        `,
-        }}
-      />
     </Helmet>
   );
 }
