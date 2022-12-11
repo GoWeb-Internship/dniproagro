89b476c911dd465b25927639c1632d029ff3ffb0
Nadiia Khoptiuk <holineinadia@gmail.com>
2022-12-11 21:45:08 +0200

    add aria-labels

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -15,7 +15,7 @@ export const Contact = ({ contactsArr }) => (
               <a
                 className={s.messengerLink}
                 href={`https://t.me/${telegram}`}
-                aria-label="telegram"
+                aria-label="telegram link"
               >
                 <div className={s.imgWpapper}>
                   <img src={telegramImg} className={s.icon} alt="telegram" />
@@ -26,7 +26,7 @@ export const Contact = ({ contactsArr }) => (
               <a
                 className={s.messengerLink}
                 href={`viber://chat?number=%2B${viber}`}
-                aria-label="viber"
+                aria-label="viber link"
               >
                 <div className={s.imgWpapper}>
                   <img src={viberImg} className={s.icon} alt="viber" />
@@ -37,7 +37,7 @@ export const Contact = ({ contactsArr }) => (
               <a
                 className={s.messengerLink}
                 href={`https://wa.me/${whatsapp}`}
-                aria-label="whatsapp"
+                aria-label="whatsapp link"
               >
                 <div className={s.imgWpapper}>
                   <img src={whatsappImg} className={s.icon} alt="whatsapp" />
